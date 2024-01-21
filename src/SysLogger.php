9524fbcfbb531b2f1fc91d7506ef61d3929c5c14
Gunturris <guntur@asi-asiapacific.com>
2024-01-21 10:08:39 +0700

    update uuid

--- src/SysLogger.php
+++ src/SysLogger.php
@@ -29,14 +29,14 @@ class SysLogger{
          
         $queue_name = 'logger-sys'; 
         $message = [
-            "time"      => date("Y-m-d\TH:i:s"),
             "remark"    => $remark,
             "level"             => $level,
             "endpoint_rules"    => $task,
+            "note"      => $note,
             "endpoint_type"     => $_ENV['PROJECT_ENDPOINT_TYPE'],
             "source"    => $_ENV['PROJECT_CODE'] ,
-            "uuid"      => md5(mt_rand(0,1000).time()),
-            "note"      => $note
+            "time"      => date("Y-m-d\TH:i:s"),
+            "uuid"      => SysLogger::uuid_v4()
         ];
         $message_json = json_encode( $message );
         $channelMQ->queue_declare($queue_name, true, false, false, false);
@@ -60,5 +60,28 @@ class SysLogger{
         ); 
         
     }
-         
+    
+    public static function uuid_v4() {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+    
+          // 32 bits for "time_low"
+          mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+    
+          // 16 bits for "time_mid"
+          mt_rand(0, 0xffff),
+    
+          // 16 bits for "time_hi_and_version",
+          // four most significant bits holds version number 4
+          mt_rand(0, 0x0fff) | 0x4000,
+    
+          // 16 bits, 8 bits for "clk_seq_hi_res",
+          // 8 bits for "clk_seq_low",
+          // two most significant bits holds zero and one for variant DCE1.1
+          mt_rand(0, 0x3fff) | 0x8000,
+    
+          // 48 bits for "node"
+          mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+      }
+
 }
