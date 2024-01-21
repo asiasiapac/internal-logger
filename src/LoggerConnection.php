e78a205ad1b6cfcce34a20a56db3168a53bbfb21
Gunturris <guntur@asi-asiapacific.com>
2024-01-21 08:38:21 +0700

    update check versi 1.0.2

--- src/LoggerConnection.php
+++ src/LoggerConnection.php
@@ -1,6 +1,6 @@
 <?php 
 
-namespace InternalLogger;
+namespace AsiAsiapac\InternalLogger;
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
@@ -13,42 +13,52 @@ class LoggerConnection{
 	public const TRACE = "TRACE";
 	public const DEBUG = "DEBUG";
 
-    private $connection, $channel = null;
+    private $connection, 
+                $channel = null;
 	 
 	public static function save( $level, $task, $remark, $note = "" ){
-        $connectMQ = $this->AmqpConnect( );
-
+         
+        $connectMQ = new AMQPStreamConnection(
+            $_ENV['RABBITMQ_HOST']  , 
+            $_ENV['RABBITMQ_PORT'] , 
+            $_ENV['RABBITMQ_USERNAME'], 
+            $_ENV['RABBITMQ_PASSWORD'],
+            $_ENV['RABBITMQ_VHOST'] 
+        ); 
+        $channelMQ = $connectMQ->channel(); 
+         
         $queue_name = 'logger-sys'; 
         $message = [
-            "time"      =>date("Y-m-d\TH:i:s"),
-            "remark"    =>$remark,
-            "level"     =>$level,
-            "endpoint_rules" => $task,
-            "endpoint_type" => 'AMQP',
-            "source" => 'WEBLMS',
-            "uuid" => md5(mt_rand(0,1000).time()),
-            "note" => $note
+            "time"      => date("Y-m-d\TH:i:s"),
+            "remark"    => $remark,
+            "level"             => $level,
+            "endpoint_rules"    => $task,
+            "endpoint_type"     => $_ENV['PROJECT_ENDPOINT_TYPE'],
+            "source"    => $_ENV['PROJECT_CODE'] ,
+            "uuid"      => md5(mt_rand(0,1000).time()),
+            "note"      => $note
         ];
         $message_json = json_encode( $message );
-        $connectMQ->channel->queue_declare($queue_name, true, false, false, false);
+        $channelMQ->queue_declare($queue_name, true, false, false, false);
 
         $msg = new AMQPMessage($message_json);
 
-        $connectMQ->channel->basic_publish($msg, '', $queue_name);
-        $connectMQ->channel->close();
-        $connectMQ->connection->close();
+        $channelMQ->basic_publish($msg, '', $queue_name);
+        $channelMQ->close();
+
+        $connectMQ->close();
         return true;
     }  
 
-    public function AmqpConnect(){
-        $this->connection =  new AMQPStreamConnection(
-            env('RABBITMQ_HOST')  , 
-            env('RABBITMQ_PORT') , 
-            env('RABBITMQ_USERNAME'), 
-            env('RABBITMQ_PASSWORD'),
-            env('RABBITMQ_VHOST') 
+    public static function AmqpConnect(){
+        return new AMQPStreamConnection(
+            $_ENV['RABBITMQ_HOST']  , 
+            $_ENV['RABBITMQ_PORT'] , 
+            $_ENV['RABBITMQ_USERNAME'], 
+            $_ENV['RABBITMQ_PASSWORD'],
+            $_ENV['RABBITMQ_VHOST'] 
         ); 
-        $this->channel = $this->connection->channel( );
+        
     }
          
 }
